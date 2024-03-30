be19b9164dd772352b7a62febe7b07907b8d9568
Snezhana Petlova <snpetlova@gmail.com>
2024-03-30 11:34:37 +0200

    create navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <Menu fixed="top" size="huge">
+      <Menu.Item as={Link} to="/"> Home </Menu.Item>
+      <Menu.Item as={Link} to="/rated"> Rated </Menu.Item>
+
+      <Menu.Menu position="right">
+        <Menu.Item as={Link} to="/auth">
+          Auth
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
