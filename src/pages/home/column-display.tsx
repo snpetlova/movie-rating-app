265aa00653c5edfe081636866e19b3233c291f6b
Snezhana Petlova <snpetlova@gmail.com>
2024-04-03 17:47:19 +0200

    implement rating

--- src/pages/home/column-display.tsx
+++ src/pages/home/column-display.tsx
@@ -1,6 +1,11 @@
-import { Card, Grid, GridColumn, Rating } from "semantic-ui-react";
+import {Card, Grid, Form} from "semantic-ui-react";
 import { DisplayType } from ".";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { rateMovie, rateTvShow } from "./mutation";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 interface DisplayData {
   id: number;
@@ -19,25 +24,74 @@ interface Props {
 
 export const ColumnDisplay = (props: Props) => {
   const { data, displayType } = props;
+  const [rating, setRating] = useState<number>(0);
+
+  const onSuccess = () => {
+    toast.success("Succesfully rated!");
+  }
+
+  const onError = () => {
+    toast.error("Something went wrong!")
+  }
+
+  const {mutate: rateMovieMutation} = useMutation({
+    mutationKey: ["rateMovie"],
+    mutationFn: (id: number) => rateMovie(id, rating),
+    onSuccess,
+    onError,
+  });
+
+  const {mutate: rateTvShowMutation} = useMutation({
+    mutationKey: ["rateTvShow"],
+    mutationFn: (id: number) => rateTvShow(id, rating),
+    onSuccess,
+    onError,
+  });
+
+  const rate = displayType === DisplayType.Movies ? rateMovieMutation : rateTvShowMutation;
 
   return (
     <Grid columns={3} stackable centered verticalAlign="top" padded>
       {data.map((displayData: DisplayData) => (
         <Grid.Column key={displayData.id}>
           <Card.Group>
-            <Link to={`/${displayType === DisplayType.Movies ? "movie" : "tvshow"}/${displayData.id}`}>
-            <Card
-              fluid
-              image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`}
-              header={
-                displayType === DisplayType.Movies
-                  ? displayData.title
-                  : displayData.name
-              }
-              meta={`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`}
-              description={displayData.overview.slice(0, 350) + "..."}
-            />
+            <Link
+              to={`/${
+                displayType === DisplayType.Movies ? "movie" : "tvshow"
+              }/${displayData.id}`}
+            >
+              <Card
+                fluid
+                image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`}
+                header={
+                  displayType === DisplayType.Movies
+                    ? displayData.title
+                    : displayData.name
+                }
+                meta={`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`}
+                description={displayData.overview.slice(0, 350) + "..."}
+              />
             </Link>
+            <Form style={{ marginTop: 10 }}>
+              <Form.Group inline>
+                <Form.Field>
+                  <Form.Input
+                    type="number"
+                    min="0"
+                    max="10"
+                    step="0.5"
+                    onChange={(e) => setRating(Number(e.target.value))}
+                    action={{
+                        color: "violet",
+                        labelPosition: "right",
+                        icon: "star",
+                        content: "Rate",
+                        onClick: () => rate(displayData.id)
+                    }}
+                  />
+                </Form.Field>
+              </Form.Group>
+            </Form>
           </Card.Group>
         </Grid.Column>
       ))}
